e9f386630b48a29ad7ba1f1b0320933939093d37
entertotheneworld <tristan.greffe@hotmail.com>
2022-04-22 13:01:41 +0200

    Amélioration des tests

--- spec/00_dark_trader_spec.rb
+++ spec/00_dark_trader_spec.rb
@@ -6,6 +6,8 @@ describe "crypto_scrapper_method_1" do
     expect(crypto_scrapper_method_1).not_to be_empty
     expect(crypto_scrapper_method_1.class).to eq(Array)
     expect(crypto_scrapper_method_1.size).to eq(20)
+    expect(crypto_scrapper_method_1.include?("BTC")).to eq(true)
+    expect(crypto_scrapper_method_1.include?("ETH")).to eq(true)
   end
 end
 
@@ -15,6 +17,8 @@ describe "crypto_scrapper_method_2" do
     expect(crypto_scrapper_method_2).not_to be_empty
     expect(crypto_scrapper_method_2.class).to eq(Array)
     expect(crypto_scrapper_method_2.size).to eq(200)
+    expect(crypto_scrapper_method_2.include?("BTC")).to eq(true)
+    expect(crypto_scrapper_method_2.include?("ETH")).to eq(true)
   end
 end
 
@@ -23,5 +27,7 @@ describe "crypto_scrapper_method_3" do
     expect(crypto_scrapper_method_3).not_to be_nil
     expect(crypto_scrapper_method_3).not_to be_empty
     expect(crypto_scrapper_method_3.class).to eq(Array)
+    expect(crypto_scrapper_method_3.include?("BTC")).to eq(true)
+    expect(crypto_scrapper_method_3.include?("ETH")).to eq(true)
   end
 end
